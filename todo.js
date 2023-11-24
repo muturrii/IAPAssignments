35333f3eebc54dbbf95c36c3dc6f57ff57bdb68d
Muturrii <122809798+muturrii@users.noreply.github.com>
2023-11-24 23:42:18 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,3 +1,18 @@
+// KARUGU ANTHONY MUTURI
+// SCT211-0016/2022
+
+// MWANGI HILDA WAIRIMU
+// SCT211-0026/2022
+
+// KIMBERLY WANGARI NJOROGE
+// SCT211-0060/2022
+
+// KIMANI OWEN MWANGI
+// SCT211-0591/2021
+
+// ROWE PEARL ROWENA
+// SCT211-0678/2021
+
 document.addEventListener('DOMContentLoaded', function() {
 
     const submit = document.querySelector('#submit');
